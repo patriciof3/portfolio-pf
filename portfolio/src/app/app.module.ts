e4066aa54da14958224351fe0b58498a2ef9c125
Patricio Ferreira <patricio.afr@gmail.com>
2022-03-21 14:26:29 -0300

    agregué sección "experiencia laboral"

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { TrabajoComponent } from './trabajo/trabajo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    TrabajoComponent
   ],
   imports: [
     BrowserModule,
